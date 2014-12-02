7dcbb346081ddc966c81fd4b8cb2b68aa7071f03
Benjy Levin <benjylevin@college.harvard.edu>
2014-12-02 02:35:22 -0500

    Added the start of detect.js - can find RSSI values of BLE devices

---
+++ detect.js
@@ -0,0 +1,44 @@
+var noble = require('noble');
+
+var dict = {};
+
+noble.on('stateChange', function(state) {
+	if (state === 'poweredOn' ) {
+		noble.startScanning([],true);
+		console.log('Started Scanning');
+	} else {
+		noble.stopScanning();
+	}
+});
+
+noble.on('discover', function(peripheral) {
+console.log('Discovered Peripheral : ' + peripheral.uuid + ' RSSI:' + peripheral.rssi);
+
+if (peripheral.uuid in dict){
+	dict[peripheral.uuid].count = dict[peripheral.uuid].count + 1;
+	dict[peripheral.uuid].average = (dict[peripheral.uuid].average*(dict[peripheral.uuid].count-1) + peripheral.rssi)/dict[peripheral.uuid].count;
+}
+else {
+	dict[peripheral.uuid] = {'average':peripheral.rssi, 'count':1};
+}
+console.log(dict)
+/*peripheral.connect(function(error){
+	if (error == undefined ){
+		console.log(peripheral.uuid + ' RSSI:' + peripheral.rssi );
+	} else {
+		console.log(peripheral.uuid + ' RSSI:' + peripheral.rssi + ' Connecting, Error : ' + error); 
+	}
+}); */
+/*peripheral.updateRssi(function(error, rssi){
+console.log(peripheral.uuid + ' RSSI:' + peripheral.rssi+ ' update RSSI + ' + rssi + ' : Error :' + error);
+});*/
+
+/*peripheral.on('connect',function(){
+	console.log(peripheral.uuid + ' RSSI:' + peripheral.rssi+ ' Has conected');
+}); */
+/*peripheral.on('rssiUpdate',function(rssi){
+console.log(peripheral.uuid + ' RSSI updated : ' + rssi);
+
+});
+*/
+});
